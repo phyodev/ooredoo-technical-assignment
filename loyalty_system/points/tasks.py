0967fb068b5c30f35361b354d29744a537717d4e
Phyo Pyae Sone <phyopyaesone.developer@gmail.com>
2025-03-25 14:56:46 +0630

    fix[Redemption]: substract product points from user total points when redeem, update readme.md

--- loyalty_system/points/tasks.py
+++ loyalty_system/points/tasks.py
@@ -1,9 +1,12 @@
 from celery import shared_task
+from django.utils import timezone
 from .models import Redemption
+from users.models import CustomUser
+from points.models import PointLedger
 import requests
 
 @shared_task
-def process_redemption(redemption_id):
+def process_redemption(redemption_id, customer_id):
     """
     Celery task to process redemption by calling the local redeem API.
     """
@@ -18,9 +21,35 @@ def process_redemption(redemption_id):
         response = requests.post(url, verify=False) # Disable SSL verification for just local development
 
         if response.status_code == 200:
-            redemption.status = "success"
-            redemption.save()
-            return f"Redemption {redemption_id} successfully redeemed!"
+            customer = CustomUser.objects.get(id=customer_id)
+
+            total_points = PointLedger.get_available_points(customer)
+            if total_points >= redemption.product.points_earned:
+                product_points = redemption.product.points_earned
+
+                # Fetch all valid PointLedger entries for the customer (order by expiry date)
+                point_ledgers = PointLedger.objects.filter(
+                    customer=customer, expires_at__gt=timezone.now()
+                ).order_by('expires_at')
+
+                # substract product points from user total points
+                substract_points = product_points
+                for ledger in point_ledgers:
+                    substract_points = substract_points - ledger.points
+                    if substract_points < 0:
+                        ledger.points = abs(substract_points)
+                        ledger.save()
+                        break
+                    elif substract_points == 0:
+                        ledger.delete()
+                        break
+                    elif substract_points > 0:
+                        ledger.delete()
+                        continue
+
+                redemption.status = "success"
+                redemption.save()
+                return f"Redemption {redemption_id} successfully redeemed!"
         else:
             redemption.status = "failed"
             redemption.save()
